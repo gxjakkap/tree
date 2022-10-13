693cd87f92a83ae07206c7943841132ce7798576
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2022-10-14 00:23:20 +0700

    fix: use cloudinary for image

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
   <div>
     <div class="w-full h-screen pt-5">
       <figure>
-        <img src="./assets/thumb-142.webp" class="drop-shadow-xl rounded-full w-[175px] ml-auto mr-auto" alt="GuntxJakka's portrait">
+        <img src="https://res.cloudinary.com/dynrld3nm/image/upload/w_175,q_50/v1665681568/tree/thumb.webp" class="drop-shadow-xl rounded-full w-[175px] ml-auto mr-auto" alt="GuntxJakka's portrait">
       </figure>
       <h1 class="font-mali text-sky-900 text-center my-3 text-5xl drop-shadow-xl">GuntxJakka</h1>
       <LinksButton/>
